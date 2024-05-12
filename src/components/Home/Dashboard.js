64c364abf8d19db9c8de0c03687a98f3d960e603
Collge <collge.io.app@gmail.com>
2024-05-12 15:41:18 +0500

    Adding initial files

---
+++ src/components/Home/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../../dist/css/Dashboard.css"
+
+function Dashboard()
+{
+    return (
+        <div>
+            <p>Dashboard</p>
+        </div>
+    )
+}
+
+export default Dashboard
